f3078e969aea130d9c83f1a8d122cbefade52e5a
CORE jake <corejake609@gmail.com>
2026-02-01 07:25:09 +0300

    feat: keycode resolution

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -426,9 +426,19 @@ impl State {
             time,
             |this, mods, keysym| {
                 let key_code = event.key_code();
-                let modified = keysym.modified_sym();
-                let raw = keysym.raw_latin_sym_or_raw_current_sym();
+                let keysym_binded = this.niri.config.borrow().input.bind_to_keysyms;
+
                 let modifiers = modifiers_from_state(*mods);
+                let modified = keysym.modified_sym();
+
+                let raw = if keysym_binded {
+                    keysym.raw_latin_sym_or_raw_current_sym()
+                } else {
+                    let xkb = keysym.xkb().lock().unwrap();
+                    xkb.raw_syms_for_key_in_layout(key_code, Layout(0))
+                        .first()
+                        .cloned()
+                };
 
                 // After updating XKB state from accessibility-grabbed keys, return right away and
                 // don't handle them.
